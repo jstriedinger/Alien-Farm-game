50f9ef5fc397bb46d8096f2af88f90fa5eca9f49
Jose Striedinger <jstriedinger2090@gmail.com>
2021-08-29 23:28:48 -0500

    Initial commit

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] bool direction;
+    private float maxX;
+    private float minX;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 esquinaInfDer = Camera.main.ViewportToWorldPoint(new Vector2(1, 0));
+        maxX = esquinaInfDer.x;
+
+        Vector2 esquinaInfIzq = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        minX = esquinaInfIzq.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction)
+            transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+        else
+            transform.Translate(new Vector2(-speed * Time.deltaTime, 0));
+
+        if (transform.position.x >= maxX)
+            direction = false;
+        else if (transform.position.x <= minX)
+            direction = true;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float nextFire;
+
+
+
+    float minX, maxX, minY, maxY, tamX, tamY, canFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tamX = (GetComponent<SpriteRenderer>()).bounds.size.x;
+        tamY = (GetComponent<SpriteRenderer>()).bounds.size.y;
+
+        Vector2 esquinaSupDer = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        maxX = esquinaSupDer.x - tamX / 2;
+        maxY = esquinaSupDer.y - tamY / 2;
+
+        Vector2 esquinaInfIzq = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        minX = esquinaInfIzq.x + tamX / 2;
+        minY = esquinaInfIzq.y + 7;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Fire();
+    }
+
+    void Movement()
+    {
+        float movH = Input.GetAxis("Horizontal");
+        float movV = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector2(movH * Time.deltaTime * speed, movV * Time.deltaTime * speed));
+
+
+        float newX = Mathf.Clamp(transform.position.x, minX, maxX);
+        float newY = Mathf.Clamp(transform.position.y, minY, maxY);
+
+        transform.position = new Vector2(newX, newY);
+    }
+
+    void Fire()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= canFire)
+        {
+            Instantiate(bullet, transform.position - new Vector3(0, tamY / 2, 0), transform.rotation);
+            canFire = Time.time + nextFire;
+
+        }
+    }
+}
